29070ea51fd21fe2e327a73d40e74288275ec41f
kyzsu <l0calh34rtz@gmail.com>
2021-10-27 07:07:06 +0700

    feat: add Login and Home Screen

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import CategoryCard from "./CategoryCard";
+import CustomButton from "./CustomButton";
+import TabIcon from "./TabIcon";
+import TrendingCard from "./TrendingCard";
+
+export { CategoryCard, CustomButton, TabIcon, TrendingCard };
